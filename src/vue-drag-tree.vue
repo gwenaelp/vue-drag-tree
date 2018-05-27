d8c276bdae5f75fd4f58a19c1bc9e49e3f700385
Gwenael Pluchon <gwenael.pluchon+github@gmail.com>
2018-05-27 05:50:05 +0200

    cleanup and adaptation for personal project

--- src/vue-drag-tree.vue
+++ src/vue-drag-tree.vue
@@ -1,14 +1,20 @@
 <template>
     <div>
-        <div :id='model.id' @click="toggle" @dblclick="changeType" draggable='true' @dragstart='dragStart' @dragover='dragOver' @dragenter='dragEnter' @dragleave='dragLeave' @drop='drop' @dragend='dragEnd' class='treeNodeText' @mouseover='mouseOver' @mouseout='mouseOut' :style='styleObj'>
-            <span :class="[isClicked ? 'nodeClicked' : '','vue-drag-node-icon']"></span>
-            {{model.name}}
-            <span @click="removeChild(model.id)" v-if='model.id !="0"'>&nbsp;x</span>
+        <div
+          :id='model.id' @click="toggle" @dblclick="changeType" draggable='true' @dragstart='dragStart' @dragover='dragOver' @dragenter='dragEnter' @dragleave='dragLeave' @drop='drop' @dragend='dragEnd' :class="selectedItem === model ? 'treeNodeText active': 'treeNodeText'" @mouseover='mouseOver' @mouseout='mouseOut' :style='styleObj' v-if="model.type">
+            <span
+              :class="[open ? 'nodeClicked' : '','vue-drag-node-icon']"
+              v-if="isFolder"
+              @click="open = !open"
+            />
+            {{model[titleKey] || '[Untitled]' }}
+            ({{model.type}})
+            <span @click="removeChild(model)" v-if='model.type'>&nbsp;x</span>
         </div>
+        <div v-else>ROOT</div>
         <div class='treeMargin' v-show="open" v-if="isFolder">
-            <item v-for="model in model.widgets" :model="model" :key='model.id' :current-highlight='currentHighlight' :default-text='defaultText' 　:hover-color='hoverColor' :highlight-color='highlightColor'>
-            </item>
-            <div class='changeTree' @click="addChild" @drop='dropPlus' @dragover='dragOverPlus' @dragenter='dragEnterPlus'>+</div>
+            <vue-drag-tree v-for="model in model[childrenKey]" :model="model" :key='model.id' :current-highlight='currentHighlight' :default-text='defaultText' :hover-color='hoverColor' :highlight-color='highlightColor' :childrenKey="childrenKey" @nodeClicked="$emit('nodeClicked', $event)" :selectedItem="selectedItem">
+            </vue-drag-tree>
         </div>
     </div>
 </template>
@@ -21,10 +27,10 @@ let fromParentModelChildren = ''  // from 节点的父组件的model
 let nodeClicked = undefined   // Attention: 递归的所有组件共享同一个＂顶级作用域＂（这个词或许不太正确，但就这个意思）．即：共享上面这几个let变量．这为实现当前节点的高亮提供了基础．
 
 export default {
-    name: 'VueDragTreeCom42',
+    name: 'vue-drag-tree',
     data: function () {
         return {
-            open: false,
+            open: true,
             isClicked: false,
             styleObj: {
                 background: 'white',
@@ -37,22 +43,30 @@ export default {
         'current-highlight': Boolean, // 当前节点高亮
         'hover-color': String,
         'highlight-color': String,
+        'childrenKey': {
+          type: String,
+          default: 'children',
+        },
+        'titleKey': {
+          type: String,
+          default: 'title',
+        },
+        selectedItem: {
+          type: Object
+        }
     },
     computed: {
         isFolder() {
-            return this.model.widgets &&
-                this.model.widgets.length
+            return this.model[this['childrenKey']] &&
+                this.model[this['childrenKey']] .length
         },
     },
     methods: {
         toggle() {
-            if (this.isFolder) {
-                this.open = !this.open
-            }
             // 调用vue-drag-tree的父组件中的方法,以传递出当前被点击的节点的id值
             let rootTree = this.findRoot()
-            //　API: 对外开放的当前被点击节点的信息
-            rootTree.$parent.curNodeClicked(this.model, this)
+
+            this.$emit('nodeClicked', this.model);
 
             // 纪录节点被点击的状态
             this.isClicked = !this.isClicked
@@ -60,7 +74,7 @@ export default {
             // 用户需要节点高亮？　-->　this.currentHighlight ? 高亮 : 不高亮
             if (this.currentHighlight) {
                 // 第一次点击当前节点．当前节点高亮，遍历重置其他节点的样式
-                if (nodeClicked != this.model.id) {
+                if (nodeClicked != this.model) {
                     let treeParent = rootTree.$parent
 
                     // 遍历重置所有树组件的高亮样式
@@ -76,54 +90,57 @@ export default {
                     this.styleObj.background = this.highlightColor ? this.highlightColor : '#99A9BF'
 
                     // 设置为当前节点
-                    nodeClicked = this.model.id
+                    nodeClicked = this.model
                 }
             }
         },
         exchangeData(rootCom, from, to) {
             //如果drag的目的地是 + - 符号的话，退出。
-            if (!to || !from || typeof to == 'string' || from.id == to.id) {
+            if (!to || !from || typeof to == 'string' || from == to) {
                 return
             }
 
             from = JSON.parse(JSON.stringify(from))
-            to = JSON.parse(JSON.stringify(to))
+            to = JSON.parse(JSON.stringify(to));
             // copy一个,最后再赋值给state.treeData.这样能保证值的变化都会触发视图刷新(因为JS判断引用类型是否相同是根据内存地址.)
-            let treeData = JSON.parse(JSON.stringify(this.model))
-            let nodeStack = [treeData]
-            let status = 0
+            let treeData = JSON.parse(JSON.stringify(this.model));
+            let nodeStack = [treeData];
+            let status = 0;
+            const childrenKey = this.childrenKey;
+
+            console.log('exchangeData', from, to, rootCom);
 
             // 如果from或者to节点存在父子/祖辈关系，会报id of undefined的错。这种情况不考虑拖拽功能，所以catch住，返回/return就行
             try {
                 // 广度优先遍历,找到涉及数据交换的两个对象.然后交换数据.
                 while (!(status === 2)) {
                     let item = nodeStack.shift()
-                    if (item.id == from.id) {
-                        item.id = to.id
-                        item.name = to.name
-                        if (to.children && to.children.length > 0) {
-                            item['children'] = to.children
+                    if (item == from) {
+                        item = to
+                        item.title = to.title
+                        if (to[childrenKey] && to[childrenKey].length > 0) {
+                            item[childrenKey] = to[childrenKey]
                         } else {
-                            item.children = []
+                            item[childrenKey] = []
                         }
                         status++
                         //找到后,跳出当前循环.
                         continue;
                     }
-                    if (item.id == to.id) {
-                        item.id = from.id
-                        item.name = from.name
-                        if (from.children && from.children.length > 0) {
-                            item['children'] = from.children
+                    if (item == to) {
+                        item = from
+                        item.title = from.title
+                        if (from[childrenKey] && from[childrenKey].length > 0) {
+                            item[childrenKey] = from[childrenKey]
                         } else {
-                            item.children = []
+                            item[childrenKey] = []
                         }
                         status++
                         //找到后,跳出当前循环.
                         continue;
                     }
-                    if (item.children && item.children.length > 0) {
-                        nodeStack = nodeStack.concat(item.children)
+                    if (item[childrenKey] && item[childrenKey].length > 0) {
+                        nodeStack = nodeStack.concat(item[childrenKey])
                     }
                 }
             } catch (e) {
@@ -135,10 +152,10 @@ export default {
         changeType() {
             // 用户需要高亮-->才纪录当前被点击节点
             if (this.currentHighlight) {
-                nodeClicked = this.model.id
+                nodeClicked = this.model
             }
             if (!this.isFolder) {
-                this.$set(this.model, 'children', [])
+                this.$set(this.model, this.childrenKey, [])
                 this.addChild()
                 this.open = true
                 this.isClicked = true
@@ -150,9 +167,7 @@ export default {
             }
         },
         mouseOut(e) {
-            if ((this.styleObj.background != '#99A9BF' || this.styleObj.background != this.hightlightColor) && e.target.className === 'treeNodeText') {
-                e.target.style.background = 'white'
-            }
+          e.target.style.background = 'white'
         },
         findRoot() {
             // 返回Tree的根,即递归Tree时的最顶层那个vue-drag-tree组件
@@ -164,7 +179,7 @@ export default {
                 // v1.0.9-update: add another judgement method.
                 if (!/VueDragTreeCom42/.test(that.$parent.$vnode.tag) || that.$parent.data) {
                     ok = true
-                    // 交换两者的数据 
+                    // 交换两者的数据
                     break
                 }
                 that = that.$parent
@@ -172,23 +187,20 @@ export default {
             return that
         },
         addChild() {
-            this.model.children.push({
-                name: this.defaultText ? this.defaultText : 'New Node',
-                id: id++
-            })
+//            this.model[this.childrenKey].push({
+//                title: this.defaultText ? this.defaultText : 'New Node',
+//            })
         },
-        removeChild(id) {
+        removeChild(item) {
             // 获取父组件的model.children
-            let parent_model_children = this.$parent.model.children
+            let parent_model_children = this.$parent.model[this.childrenKey]
 
-            // 在父组件model.children里删除
-            for (let index in parent_model_children) {
-                // 找到该删的id
-                if (parent_model_children[index].id == id) {
-                    parent_model_children = parent_model_children.splice(index, 1)
-                    break
-                }
+            var i = parent_model_children.indexOf(item);
+            if(i !== -1) {
+              parent_model_children.splice(i, 1);
             }
+
+            this.$parent.model[this.childrenKey] = [...parent_model_children];
         },
         dragStart(e) {
             // fromData = this.model
@@ -216,35 +228,34 @@ export default {
             rootTree.exchangeData(rootTree.$parent, fromData, toData)
         },
         dragLeave(e) {
+          if(this.$parent.model) {
             fromData = this.model
-            fromParentModelChildren = this.$parent.model.children
-            // e.target.style.background = '#7B1FA2'
+            fromParentModelChildren = this.$parent.model[this.childrenKey]
+            //e.target.style.background = '#7B1FA2';
+          }
         },
         drop(e) {
-            // toData = this.model
+            toData = this.model
             // e.target.style.background = '#7B1FA2'
         },
         dropPlus(e) {
             // 把from节点插入到当前层级节点的最后一个
-            if (this.model.hasOwnProperty('children')) {
-                this.model.children.push(fromData)
+            if (this.model.hasOwnProperty(this.childrenKey)) {
+                this.model[this.childrenKey].push(fromData)
             } else {
-                this.model.children = [fromData]
+                this.model[this.childrenKey] = [fromData]
             }
 
             // 把from节点从之前的节点删除
             for (let i in fromParentModelChildren) {
-                if (fromParentModelChildren[i].id == fromData.id) {
+                if (fromParentModelChildren[i] == fromData) {
                     fromParentModelChildren.splice(i, 1)
                 }
             }
         }
     },
     beforeCreate() {
-        this.$options.components.item = require('./vue-drag-tree')
-    },
-    created() {
-        // console.log('this.hig', this.highlightColor, '|', this.hoverColor)
+      this.$options.components['vue-drag-tree2'] = require('./vue-drag-tree');
     },
 }
 </script>
@@ -263,12 +274,15 @@ export default {
     padding: 0.2rem 0.5rem;
     width: fit-content;
     background: #F9FAFC;
-    font-size: 18px;
     color: #324057;
 }
 
+.active {
+    background: #CCCCCC!important;
+}
+
 .treeMargin {
-    margin-left: 2rem;
+    margin-left: 1rem;
 }
 
 .changeTree {
